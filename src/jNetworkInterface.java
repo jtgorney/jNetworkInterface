c10d860b5d2c9f6b42b52e80e15a7745a6d1a641
Jacob Gorney <jtgorney@svsu.edu>
2014-08-31 16:00:19 -0400

    Added a function to determine the quality. It is preliminary.

--- src/jNetworkInterface.java
+++ src/jNetworkInterface.java
@@ -1,5 +1,20 @@
-import java.io.IOException;
-import java.net.Socket;
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.io.*;
+import java.net.*;
 
 /**
  * @author Jacob Gorney
@@ -74,6 +89,17 @@ public class jNetworkInterface {
    }
 
    /**
+    * Get the raw socket.
+    * @return Socket connection. Null if not available.
+    */
+   public Socket getConnection() {
+      if (socket != null && isConnected())
+         return socket;
+      else
+         return null;
+   }
+
+   /**
     * Check the connection status of the network connection.
     * @return Connection status
     */
@@ -88,7 +114,56 @@ public class jNetworkInterface {
     * Determine the connection quality.
     */
    public void pollQuality() {
-
+      // First the server must support the operation
+      // we are going to send to it. The "poll" command.
+      try {
+         DataOutputStream socketOut = new DataOutputStream(
+                 socket.getOutputStream());
+         BufferedReader socketIn = new BufferedReader(
+                 new InputStreamReader(socket.getInputStream()));
+         // Send the ping message with the current time in miliseconds.
+         // The response will then be compared.
+         socketOut.writeUTF("ping");
+         long responseTimeStart = System.currentTimeMillis();
+         socketOut.writeLong(responseTimeStart);
+         // Get the response message and parse it
+         long responseTime;
+         // Check if the ping command is accepted. If it is not
+         // we cannot determine the connection quality. No soup for you.
+         try {
+            responseTime = Long.parseLong(socketIn.readLine());
+         } catch (NumberFormatException ex) {
+            quality = 0;
+            return;
+         }
+         long difference = responseTime - responseTimeStart;
+         // Determine the connection quality based on the difference
+         // of the two timestamps.
+         if (difference < 1)
+            quality = 100;
+         else if (difference > 1 && difference < 5)
+            quality = 90;
+         else if (difference > 5 && difference < 20)
+            quality = 80;
+         else if (difference > 20 && difference < 100)
+            quality = 70;
+         else if (difference > 100 && difference < 200)
+            quality = 60;
+         else if (difference > 200 && difference < 500)
+            quality = 50;
+         else if (difference > 500 && difference < 1000)
+            quality = 40;
+         else if (difference > 1000 && difference < 2000)
+            quality = 30;
+         else if (difference > 2000 && difference < 5000)
+            quality = 20;
+         else if (difference > 5000 && difference < 10000)
+            quality = 10;
+         else
+            quality = 0;
+      } catch (IOException ex) {
+         quality = 0;
+      }
    }
 
    /**
