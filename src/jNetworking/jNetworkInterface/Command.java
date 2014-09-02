7225942ff55bcd4061662592353200428e6e902a
Jacob Gorney <jtgorney@svsu.edu>
2014-09-02 13:17:13 -0400

    Saving progress.

---
+++ src/jNetworking/jNetworkInterface/Command.java
@@ -0,0 +1,9 @@
+package jNetworking.jNetworkInterface;
+
+/**
+ * Created by Jacob on 9/2/2014.
+ */
+public interface Command {
+   public abstract void setup();
+   public abstract String command();
+}
