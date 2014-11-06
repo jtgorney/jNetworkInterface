74c93d7bc1c9214af755ccf41546b78ad743046f
Jacob Gorney <jacob@jacobgorney.com>
2014-11-05 23:06:18 -0500

    Added javadoc to command class.

--- src/jNetworking/jNetworkInterface/Command.java
+++ src/jNetworking/jNetworkInterface/Command.java
@@ -4,9 +4,19 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 /**
- * Created by Jacob on 9/2/2014.
+ * Interface class for commands.
  */
 public interface Command {
+    /**
+     * Setup method for pre-execution code.
+     * @param input Input parameters
+     * @param client Input socket
+     */
     public abstract void setup(ArrayList<String> input, Socket client);
+
+    /**
+     * Command execution. The 'Workhorse' of the command.
+     * @return Result
+     */
     public abstract String run();
 }
